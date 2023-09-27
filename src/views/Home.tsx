74caf20b0304e8f3279ee4c89551f7dccd6fb1d4
曹珂俭 <1849201815@qq.com>
2023-09-27 16:52:42 +0800

    feat: 完成翻页

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,40 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 import type { FC, ReactNode } from 'react'
+import s from './Home.module.scss'
 interface IProps {
   children?: ReactNode
 }
 
 const Home: FC<IProps> = () => {
+  const [aniState, setAniState] = useState('paused')
+  setTimeout(() => {
+    setAniState('running')
+  }, 2000);
   return (
-    <div>Home</div>
+    <div className={s.wrapper}>
+      <div className={s.context}>
+        上岸第一事
+        先斩<span className={s.fake}>意中人</span>
+        <p>又想吵架了？</p>
+      </div>
+      <div className={s.content}>
+        <div className={s.hoke} 
+          style={{
+            animationPlayState: aniState
+          }}>
+        </div>
+      </div>
+      {/* <div className={s.snow}> </div>
+      <div className={s.snow}> </div>
+      <div className={s.snow}> </div>
+      <div className={s.snow}> </div>
+      <div className={s.snow}> </div>
+      <div className={s.snow}> </div>
+      <div className={s.snow}> </div>
+      <div className={s.snow}> </div>
+      <div className={s.snow}> </div>
+      <div className={s.snow}> </div> */}
+    </div>
   )
 }
 
